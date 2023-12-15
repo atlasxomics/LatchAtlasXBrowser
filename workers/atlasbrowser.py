51e036f11e6a1cab29c5a40a21ddabf4f4f78cf2
Joshua Barnett <joshuab@atlasxomics.com>
2023-12-15 10:32:51 -0500

    remove saving of pods

--- workers/atlasbrowser.py
+++ workers/atlasbrowser.py
@@ -80,7 +80,7 @@ def generate_spatial(self, qcparams, **kwargs):
     ##/root/Images
     local_spatial_dir = Path(temp_dir).joinpath(spatial_dir)
     La_local_spatial_dir = Path(latch_dir).joinpath(La_spatial_dir)
-    if not local_spatial_dir.exists(): local_spatial_dir.mkdir(parents=True, exist_ok=True)
+    # if not local_spatial_dir.exists(): local_spatial_dir.mkdir(parents=True, exist_ok=True)
     try:
         if not La_local_spatial_dir.exists(): La_local_spatial_dir.mkdir(parents=True, exist_ok=True)
     except:
@@ -88,7 +88,7 @@ def generate_spatial(self, qcparams, **kwargs):
     
     local_figure_dir = Path(temp_dir).joinpath(figure_dir)
     La_local_figure_dir = Path(latch_dir).joinpath(La_figure_dir)
-    if not local_figure_dir.exists(): local_figure_dir.mkdir(parents=True, exist_ok=True)
+    # if not local_figure_dir.exists(): local_figure_dir.mkdir(parents=True, exist_ok=True)
     try:
         if not La_local_figure_dir.exists(): La_local_figure_dir.mkdir(parents=True, exist_ok=True)
     except:
@@ -109,7 +109,7 @@ def generate_spatial(self, qcparams, **kwargs):
     La_local_metadata_filename = La_local_spatial_dir.joinpath('metadata.json')
     local_scalefactors_filename = local_spatial_dir.joinpath('scalefactors_json.json')
     La_local_scalefactors_filename = La_local_spatial_dir.joinpath('scalefactors_json.json')
-    json.dump(metadata, open(local_metadata_filename,'w'), indent=4,sort_keys=True)
+    # json.dump(metadata, open(local_metadata_filename,'w'), indent=4,sort_keys=True)
     try:
         json.dump(metadata, open(La_local_metadata_filename,'w'), indent=4,sort_keys=True)
     except:
@@ -149,8 +149,8 @@ def generate_spatial(self, qcparams, **kwargs):
         tempName_postB = local_figure_dir.joinpath("postB.tif")
         La_tempName_bsa = La_local_figure_dir.joinpath("postB_BSA.tif")
         La_tempName_postB = La_local_figure_dir.joinpath("postB.tif")
-        cropped_bsa.save(tempName_bsa.__str__())
-        cropped_postB.save(tempName_postB.__str__())
+        # cropped_bsa.save(tempName_bsa.__str__())
+        # cropped_postB.save(tempName_postB.__str__())
         try:
             cropped_bsa.save(La_tempName_bsa.__str__())
         except:
@@ -178,8 +178,8 @@ def generate_spatial(self, qcparams, **kwargs):
         local_lowres_image_path = local_spatial_dir.joinpath('tissue_lowres_image.png')
         La_local_hires_image_path = La_local_spatial_dir.joinpath('tissue_hires_image.png')
         La_local_lowres_image_path = La_local_spatial_dir.joinpath('tissue_lowres_image.png')
-        high_res.save(local_hires_image_path.__str__())
-        low_res.save(local_lowres_image_path.__str__())
+        # high_res.save(local_hires_image_path.__str__())
+        # low_res.save(local_lowres_image_path.__str__())
         try:
             high_res.save(La_local_hires_image_path.__str__())   
         except:
@@ -192,7 +192,7 @@ def generate_spatial(self, qcparams, **kwargs):
         scalefactors["tissue_hires_scalef"] = factorHigh
         scalefactors["tissue_lowres_scalef"] = factorLow
     
-        json.dump(scalefactors, open(local_scalefactors_filename,'w'), indent=4,sort_keys=True)
+        # json.dump(scalefactors, open(local_scalefactors_filename,'w'), indent=4,sort_keys=True)
         try:
           json.dump(scalefactors, open(La_local_scalefactors_filename,'w'), indent=4,sort_keys=True)  
         except:
@@ -204,25 +204,21 @@ def generate_spatial(self, qcparams, **kwargs):
     La_local_tissue_positions_filename= local_spatial_dir.joinpath('tissue_positions_list.csv')
     tissue_positions_list = []
     tixel_pos_list= [x['position'] for x  in tixel_positions]
-    f=open(local_tissue_positions_filename, 'w')
-    csvwriter = csv.writer(f, delimiter=',',escapechar=' ',quoting=csv.QUOTE_NONE)
-    for idx, b in enumerate(barcodes):
-        colidx = int(idx/row_count)
-        rowidx = idx % row_count
-        keyindex = tixel_pos_list.index([rowidx,colidx])
-        coord_x = int(round(tixel_positions[keyindex]['coordinates']['x']))
-        coord_y = int(round(tixel_positions[keyindex]['coordinates']['y']))
-        val = 0
-        if tixel_positions[keyindex]['value'] : val = 1
-        datarow = [b, val, rowidx, colidx, coord_y , coord_x ]
-        tissue_positions_list.append(datarow)    
-        csvwriter.writerow(datarow)
-    f.close()
-    try:
-      f1=open(La_local_tissue_positions_filename, 'w')
-    except:
-        pass
-    
+    # f=open(local_tissue_positions_filename, 'w')
+    # csvwriter = csv.writer(f, delimiter=',',escapechar=' ',quoting=csv.QUOTE_NONE)
+    # for idx, b in enumerate(barcodes):
+    #     colidx = int(idx/row_count)
+    #     rowidx = idx % row_count
+    #     keyindex = tixel_pos_list.index([rowidx,colidx])
+    #     coord_x = int(round(tixel_positions[keyindex]['coordinates']['x']))
+    #     coord_y = int(round(tixel_positions[keyindex]['coordinates']['y']))
+    #     val = 0
+    #     if tixel_positions[keyindex]['value'] : val = 1
+    #     datarow = [b, val, rowidx, colidx, coord_y , coord_x ]
+    #     tissue_positions_list.append(datarow)    
+    #     csvwriter.writerow(datarow)
+    # f.close()
+    f1=open(La_local_tissue_positions_filename, 'w')
     csvwriter = csv.writer(f1, delimiter=',',escapechar=' ',quoting=csv.QUOTE_NONE)
     for idx, b in enumerate(barcodes):
         colidx = int(idx/row_count)
