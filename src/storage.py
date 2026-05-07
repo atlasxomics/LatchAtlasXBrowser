f22aac90944cab1ffb44be67679824d862c4c4c5
jpmcga <jpaulmcgann@gmail.com>
2026-05-07 16:43:06 -0400

    able to handle greyscale

--- src/storage.py
+++ src/storage.py
@@ -296,11 +296,21 @@ class StorageAPI:
     def crop_image(self,img, x1, x2, y1, y2):
         return img[y1: y2, x1: x2]
 
+    def extract_gray_channel(self, img):
+        if img is None:
+            raise ValueError("Unable to read image")
+        if img.ndim == 2:
+            return img
+        if img.ndim == 3:
+            return img[:, :, 0]
+
+        raise ValueError(f"Unsupported image shape: {img.shape}")
+
     def get_gray_image_rotation_cropping_jpg(self, filename, rotation, x1, x2, y1, y2):
         rel_path = Path(filename)
         path = self.ldataDirectory.joinpath(rel_path)
         img=cv2.imread(path.__str__(),cv2.IMREAD_COLOR)
-        gray_img = img[:, :, 0]
+        gray_img = self.extract_gray_channel(img)
         if rotation != 0:
             gray_img = self.rotate_image_no_cropping(gray_img, rotation)
         cropped = self.crop_image(gray_img, x1, x2, y1, y2)
@@ -312,7 +322,7 @@ class StorageAPI:
         rel_path = Path(filename)
         path = self.ldataDirectory.joinpath(rel_path)
         img=cv2.imread(path.__str__(),cv2.IMREAD_COLOR)
-        gray_img = img[:, :, 0]
+        gray_img = self.extract_gray_channel(img)
         if rotation != 0:
             gray_img = self.rotate_image_no_cropping(gray_img, rotation)
         bytesIO = self.get_img_bytes(gray_img)
@@ -376,17 +386,15 @@ class StorageAPI:
     def getFileList(self, root_path, fltr=None, only_files = False): #get all pages
       #alter this to be a lambda function that filters based on the filters and also whether the object is a file or a folder
       def checkList(roots, value, listt):
-          def checkList(roots, value, listt):
         full_path = roots.__str__() + '/' + value
         #can exclude an option if it is only looking for files and finds a folder
-        if only_files and value.is_dir():
+        if only_files and Path(full_path).is_dir():
           return False
         if fltr is not None:
           for root, dirs, files in os.walk(full_path):
             for file_name in files:
               # Get the full path of the file and append it to the list
               for search_word in listt:
-                  for search_word in listt:
                 full_path = os.path.join(root, file_name)
                 if search_word.lower() in full_path.lower():
                   return True
@@ -410,4 +418,3 @@ class StorageAPI:
       except:
         return False
     
-
