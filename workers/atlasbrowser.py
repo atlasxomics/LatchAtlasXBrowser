36ffca0355ec9d4fe4d8ae5536decf407d236f3d
jpmcga <jpaulmcgann@gmail.com>
2026-05-08 01:13:00 +0000

    Ensure grayscale fix

--- workers/atlasbrowser.py
+++ workers/atlasbrowser.py
@@ -1,48 +1,40 @@
-import csv
-import cv2
-import json
-import math
-import numpy as np
+
 import os
 import subprocess
-import utils
-import yaml
-
-from pathlib import Path
+import yaml,json,csv
 from PIL import Image
-
+from pathlib import Path
+import json
+from pathlib import Path
+Image.MAX_IMAGE_PIXELS = None
 from celery import Celery
 from celery.signals import worker_process_init
+import time
+import shutil
+import math
+import numpy as np 
+import utils
+import cv2
 
-
-Image.MAX_IMAGE_PIXELS = None
-
-app = Celery(
-    'atlasbrowser_task',
-    broker='amqp://guest:guest@localhost:5672',
-    backend='redis://localhost:6379'
-)
-
+app=Celery('atlasbrowser_task',broker='amqp://guest:guest@localhost:5672',backend='redis://localhost:6379')
 
 @worker_process_init.connect()
 def on_worker_init(**_):
     print("AtlasBrowser Worker initiated")
 
-
 @app.task(bind=True)
 def task_list(self, *args, **kwargs):
     metafilename = Path(__file__).stem+".yml"
-    taskobject = yaml.safe_load(open(metafilename, 'r'))
+    taskobject = yaml.safe_load(open(metafilename,'r'))
     return taskobject
-
 
 def rotate_image_no_cropping(img, degree):
     (h, w) = img.shape[:2]
     (cX, cY) = (w // 2, h // 2)
     # rotate our image by 45 degrees around the center of the image
     M = cv2.getRotationMatrix2D((cX, cY), degree, 1.0)
-    abs_cos = abs(M[0, 0])
-    abs_sin = abs(M[0, 1])
+    abs_cos = abs(M[0,0]) 
+    abs_sin = abs(M[0,1])
     bound_w = int(h * abs_sin + w * abs_cos)
     bound_h = int(h * abs_cos + w * abs_sin)
     M[0, 2] += bound_w/2 - cX
@@ -62,16 +54,12 @@ def extract_postb_channel(img_arr):
 @app.task(bind=True)
 def generate_spatial(self, qcparams, **kwargs):
     self.update_state(state="STARTED")
-    self.update_state(
-        state="PROGRESS", meta={"position": "preparation", "progress": 0}
-    )
-    config = utils.load_configuration()
-
-    # config
+    self.update_state(state="PROGRESS", meta={"position": "preparation" , "progress" : 0})
+    config=utils.load_configuration()
+    ## config
     temp_dir = config['TEMP_DIRECTORY']
     latch_dir = '/ldata/spatials/'
-
-    # parameter parsing
+    ## parameter parsing
     root_dir = '/root/LatchAtlasXBrowser/Images'
     metadata = qcparams['metadata']
     oldFiles = qcparams['files']
@@ -84,91 +72,80 @@ def generate_spatial(self, qcparams, **kwargs):
     bsa_path = qcparams['bsa_path']
     barcode_path = qcparams['barcode_path']
     postB_flag = qcparams['postB_flag']
-    postb_path = "{}{}/".format(latch_dir, run_id)
-
+    postb_path = "{}{}/".format(latch_dir,run_id)
+    
     updating_existing = qcparams.get('updating_existing', False)
 
     next_gen_barcodes = True
-
+    
     metadata["replaced_24_barcodes"] = next_gen_barcodes
 
-    # source image path
-    allFiles = [i for i in oldFiles if '.json' not in i
-                and 'spatial' not in i.split('/')[:-1]]
-
-    # output directories (S3)
-    # Images
+    ### source image path
+    allFiles = [i for i in oldFiles if '.json' not in i and 'spatial' not in i]
+    ### output directories (S3)
+    ##Images
     spatial_dir = Path(root_dir).joinpath(run_id, 'spatial')
     figure_dir = Path(root_dir).joinpath(run_id, 'spatial', 'figure')
+    # if not figure_dir.exists(): figure_dir.mkdir(parents=True, exist_ok=True)
+    if not spatial_dir.exists(): spatial_dir.mkdir(parents=True, exist_ok=True)
+    if not figure_dir.exists(): figure_dir.mkdir(parents=True, exist_ok=True)
 
-    if not spatial_dir.exists():
-        spatial_dir.mkdir(parents=True, exist_ok=True)
-    if not figure_dir.exists():
-        figure_dir.mkdir(parents=True, exist_ok=True)
+    ### read barcodes information 
+    
 
-    # read barcodes information
-    self.update_state(
-        state="PROGRESS", meta={"position": "running", "progress": 20}
-    )
+    self.update_state(state="PROGRESS", meta={"position": "running" , "progress" : 20})
     barcodes = []
-
     try:
-        with open(latch_dir + barcode_path, 'r') as f:
+        with open(latch_dir + barcode_path,'r') as f:
             barcodes = f.read().splitlines()
     except:
         pass
     row_count = math.sqrt(len(barcodes))
 
-    # save metadata & scalefactors
+    ### save metadata & scalefactors
     local_metadata_filename = spatial_dir.joinpath('metadata.json')
     local_scalefactors_filename = spatial_dir.joinpath('scalefactors_json.json')
-
-    json.dump(
-        metadata, open(local_metadata_filename, 'w'), indent=4, sort_keys=True
-    )
-
+    # json.dump(metadata, open(local_metadata_filename,'w'), indent=4,sort_keys=True)
+    json.dump(metadata, open(local_metadata_filename,'w'), indent=4,sort_keys=True)
     # adding metadata and scalefactors to the list to be uploaded to S3 Bucket
+    
     if not updating_existing:
-    # load image from s3
+    ### load image from s3
         for i in allFiles:
-            vals = i.split("/")
-            name = vals[len(vals) - 1]
-            if "flow" in i.lower() or "fix" in i.lower():
-                try:
-                    os.rename(
-                        "{}/{}/{}".format(latch_dir, run_id, name), str(figure_dir.joinpath(name))
-                    )
-                except:
-                    pass
-            elif i in bsa_path:
-                bsa_original = Image.open(bsa_path)
-                bsa_img_arr = np.array(bsa_original, np.uint8)
-                bsa_original.close()
-                if rotation != 0:
-                    bsa_img_arr = rotate_image_no_cropping(bsa_img_arr, rotation)
-
-                postB_img_arr = bsa_img_arr[:, :, 2]
-                postB_source = Image.fromarray(postB_img_arr)
-                bsa_source = Image.fromarray(bsa_img_arr)
+          vals = i.split("/")
+          name = vals[len(vals) - 1]
+          if "flow" in i.lower() or "fix" in i.lower():
+            try:
+                os.rename("{}/{}/{}".format(latch_dir,run_id,name), str(figure_dir.joinpath(name)))
+            except:
+                pass
+          elif i in bsa_path:
+              bsa_original = Image.open(bsa_path)
+              bsa_img_arr = np.array(bsa_original, np.uint8)
+              bsa_original.close()
+              if rotation != 0 :
+                  bsa_img_arr = rotate_image_no_cropping(bsa_img_arr, rotation)
+                  
+              postB_img_arr = extract_postb_channel(bsa_img_arr)
+              postB_source = Image.fromarray(postB_img_arr)
+              bsa_source = Image.fromarray(bsa_img_arr)
+              
 
         self.update_state(state="PROGRESS", meta={"position": "running" , "progress" : 45})
 
-        # generate cropped images using crop parameters
-        cropped_bsa = bsa_source.crop(
-            (crop_coordinates[0], crop_coordinates[1], crop_coordinates[2], crop_coordinates[3])
-        )
+        ## generate cropped images using crop parameters
+        cropped_bsa = bsa_source.crop((crop_coordinates[0], crop_coordinates[1], crop_coordinates[2], crop_coordinates[3]))
         cropped_postB = postB_source.crop((crop_coordinates[0], crop_coordinates[1], crop_coordinates[2], crop_coordinates[3]))
-    
         ## high resolution
         tempName_bsa = figure_dir.joinpath("postB_BSA.tif")
         tempName_postB = figure_dir.joinpath("postB.tif")
-
+        
         cropped_bsa.save(tempName_bsa.__str__())
         cropped_postB.save(tempName_postB.__str__())
-
+        
         height = cropped_postB.height
         width = cropped_postB.width
-        self.update_state(state="PROGRESS", meta={"position": "running", "progress": 65})
+        self.update_state(state="PROGRESS", meta={"position": "running" , "progress" : 65})
         if width > height:
             factorHigh = 2000/width
             factorLow = 600/width
@@ -211,9 +188,8 @@ def generate_spatial(self, qcparams, **kwargs):
         csvwriter.writerow(datarow)
     f.close()
     self.update_state(state="PROGRESS", meta={"position": "Finishing" , "progress" : 80})
-
-    # concatenate tissue_positions_to gene expressions
+    ### concatenate tissue_positions_to gene expressions
     command = f"latch cp /root/LatchAtlasXBrowser/Images/{run_id}/spatial latch:///spatials/{run_id} && rm -rf /root/LatchAtlasXBrowser/Images/{run_id}"
     subprocess.run(command, shell=True)
-    self.update_state(state="PROGRESS", meta={"position": "Finished", "progress" : 100})
+    self.update_state(state="PROGRESS", meta={"position": "Finished" , "progress" : 100})
     return 'Finished'
